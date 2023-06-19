0296e9ae48213f6fb9bbd63fafc44047935839e2
ValPumpkins <quignon.val@gmail.com>
2023-06-19 03:25:05 -0700

    4-print_alphabt.c : alphabet in lowercase without q & e-Updated BettyStyle

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description:prints alphabet in lowercase without q & e
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-        {
-            putchar(letter);
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
