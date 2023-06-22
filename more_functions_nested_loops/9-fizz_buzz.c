65d3fb665484c1021b30d8d44d601a385ebe2517
ValPumpkins <quignon.val@gmail.com>
2023-06-22 06:02:27 -0700

    Fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print from 1 to 100 but print
+ * Fizz for multiples of 3
+ * Buzz for mUltiples of 5
+ * FizzBuzz for multoples of 3 & 5
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
