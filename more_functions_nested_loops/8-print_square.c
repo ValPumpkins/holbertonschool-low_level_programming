46dba28ff48cfd217abe4e39ed95bdadc16faa96
ValPumpkins <quignon.val@gmail.com>
2023-06-22 05:35:03 -0700

    8-print_square.c: prints a square-Update BettyStyle

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,13 +2,13 @@
 
 /**
  * print_square - prints a square
- * @n: number of times the character \ should be printed
+ * @size: size of the square
  * Return: Always 0
  */
 void print_square(int size)
 {
 	int row, col;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
