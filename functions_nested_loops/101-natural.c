580c3a958136f44f82a53613893e787b1b70af8f
ValPumpkins <quignon.val@gmail.com>
2023-06-23 02:21:26 -0700

    101-natural.c - updated BettyStyle

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int sum = 0;
 	int i;
-	
+
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
@@ -18,5 +18,5 @@ int main(void)
 		}
 	}
 	printf("%d\n", sum);
-	return 0;
+	return (0);
 }
