49b36584f9cd63d4f6113d399057025822e868f7
ValPumpkins <quignon.val@gmail.com>
2023-06-26 02:26:28 -0700

    Add 1-swap.c : swaps the value of 2 int

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of 2 integers
+ * @a: pointer
+ * @b: pointer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,4 +7,10 @@
 
 void reset_to_98(int *n);
 
+/**
+ * void swap_int(int *a, int *b) - swaps the values of 2 integers
+ */
+
+void swap_int(int *a, int *b);
+
 #endif
