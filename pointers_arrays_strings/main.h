2715398d6c0cfe40bb5c17c36332fdec5f7c1a67
ValPumpkins <quignon.val@gmail.com>
2023-06-27 04:43:27 -0700

    2-strncpy.c - copy a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -85,4 +85,10 @@ char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
 
+/**
+ * char *_strncpy(char *dest, char *src, int n) - copies a string
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+
 #endif
