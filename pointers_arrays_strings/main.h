0bc542f2daafc783c538c2e8e3c331972ff6e7ab
ValPumpkins <quignon.val@gmail.com>
2023-06-27 06:00:42 -0700

    3-strcmp.c: compare

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -91,4 +91,10 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ * int _strcmp(char *s1, char *s2) - compares 2 strings
+ */
+
+int _strcmp(char *s1, char *s2);
+
 #endif
