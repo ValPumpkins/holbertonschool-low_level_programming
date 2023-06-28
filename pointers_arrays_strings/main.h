61efc062a26ee253a001b8b2dd09fe86d7bd1171
ValPumpkins <quignon.val@gmail.com>
2023-06-28 06:25:31 -0700

    1-memcpy.c : copies memory area

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -127,4 +127,9 @@ char *leet(char *);
 
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * char *_memcpy(char *dest, char *src, unsigned int n) - copies memory area
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
