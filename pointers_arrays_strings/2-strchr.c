14475eb9a95901c64f202515756c2505f3bbfb69
ValPumpkins <quignon.val@gmail.com>
2023-06-28 07:44:42 -0700

    2-strchr : locate a char in a string - update NULL + For

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,19 +2,20 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: string
- * @c: char
- * Return: pointer to char in s
+ * @s: string to search
+ * @c: char to locate
+ * Return: pointer to char in s if c is found & NULL is c is not found
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
 	}
 	return ('\0');
 }
