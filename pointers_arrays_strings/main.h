335d6c4253934379f2bdae1618900782b172d5ce
ValPumpkins <quignon.val@gmail.com>
2023-06-28 07:22:21 -0700

    2-strchr : locate a char in a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -132,4 +132,11 @@ char *_memset(char *s, char b, unsigned int n);
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+ * char *_strchr(char *s, char c) - locates a character in a string
+ */
+
+char *_strchr(char *s, char c);
+
 #endif
