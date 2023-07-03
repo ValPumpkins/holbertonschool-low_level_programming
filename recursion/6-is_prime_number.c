a3fde1ddd6ddefa993194417c5b33beccca9e71c
ValPumpkins <quignon.val@gmail.com>
2023-07-03 08:07:36 -0700

    task 5 - maj no loop

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,24 +1,33 @@
 #include "main.h"
 
 /**
+ * help_prime - check for prime nb
+ * @num: number
+ * @i: index
+ * Return: prime nb
+ */
+int help_prime(int num, int i)
+{
+	if (num < 2)
+	{
+		return (0);
+	}
+	if (i * i > num)
+	{
+		return (1);
+	}
+	if (num % i == 0)
+	{
+		return (0);
+	}
+	return (help_prime(num, i + 1));
+}
+/**
  * is_prime_number - returns 1 if the int is a prime number, otherwise return 0
  * @n: number to check
  * Return: 1 if n is a prime number, if not 0
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (help_prime(n, 2));
 }
