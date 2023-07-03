d6794b0cec22d41940e770464a9d2eda441e1e02
ValPumpkins <quignon.val@gmail.com>
2023-07-03 01:29:36 -0700

    first commit for recursion + task 0

---
+++ recursion/README.md
@@ -0,0 +1,5 @@
+<h1>C - Recursion</h1>
+<h2>Holberton Programm, Week 5</h2>
+<ul>
+
+</ul>
---
+++ recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+
+int _putchar(char c);
+
+/**
+ * void _puts_recursion(char *s) - prints a string
+ */
+
+void _puts_recursion(char *s);
+
+#endif
