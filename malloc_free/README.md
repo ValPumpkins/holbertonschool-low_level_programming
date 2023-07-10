5a044e9e80b89287221515a5784f731ec5479f94
ValPumpkins <quignon.val@gmail.com>
2023-07-10 00:52:07 -0700

    Create dir malloc + task 0

---
+++ malloc_free/README.md
@@ -0,0 +1,5 @@
+<h1>C - malloc, free</h1>
+<h2>Holberton Programm, Week 6</h2>
+<ul>
+<li><strong>0-create_array.c</strong> : creates an array of chars, and initializes it with a specific char</li>
+</ul>
---
+++ malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+
+int _putchar(char c);
+
+/**
+ * char *create_array(unsigned int size, char c) - create an array of chars and initializes it with a specific char
+ */
+
+char *create_array(unsigned int size, char c);
+
+#endif
