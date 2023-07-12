3b2666f43387303976e18d424595ec4247c071a8
ValentinePumpkins <quignon.val@gmail.com>
2023-07-12 10:35:05 +0200

    test vs commit

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -19,4 +19,35 @@ int _strlen(char *s)
 }
 
 /**
- *
+ * argstostr - concactenates the arguments of your programm
+ * @ac:
+ * @av:
+ * Return: pointer to new string or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int arglen = 0;
+	int i, j;
+	char *result;
+	int count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		arglen += _strlen(av[i]) + 1;
+	}
+		result = malloc((arglen + 1) * sizeof(char));
+		if (result == NULL)
+			return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[count++] = av[i][j];
+		}
+		result[count++] = '\n';
+	}
+	result[count] = '\0';
+	return (result);
+}
