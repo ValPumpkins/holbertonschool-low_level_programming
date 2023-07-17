cfe860012b7e620564d3ae8d8f284921b827113a
ValentinePumpkins <quignon.val@gmail.com>
2023-07-17 15:45:07 +0200

    task4/updated/add free)

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -65,6 +65,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (newdog->owner == NULL)
 	{
+		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
