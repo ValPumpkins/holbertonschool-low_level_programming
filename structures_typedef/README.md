06fea59cf89d7e1b449b2778908f3896a64b2476
ValentinePumpkins <quignon.val@gmail.com>
2023-07-17 11:03:14 +0200

    Created dir struc/typedef + task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,5 @@
+<h1>C - Structures_typedef</h1>
+<h2>Holberton Programm, Week 7</h2>
+<ul>
+
+</ul>
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - data for dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Description: data for dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
