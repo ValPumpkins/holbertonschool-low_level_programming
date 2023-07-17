b0d7b6b98a6408638873d0eb0c49897420c6e891
ValentinePumpkins <quignon.val@gmail.com>
2023-07-17 17:13:52 +0200

    task 1 function execute a function

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes a function as a parameter in each elet of array
+* @array: array of int
+* @size: size of the array
+* @action: pointer to the function needed
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
