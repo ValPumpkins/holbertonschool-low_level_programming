6491707d87c90a38c646720d2b4eacbeb874ea28
ValentinePumpkins <quignon.val@gmail.com>
2023-07-18 10:07:51 +0200

    task 3 updated main : add check nb opérateur

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
 	op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL)
+	if ((op_func == NULL) || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
