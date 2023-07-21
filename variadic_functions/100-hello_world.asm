ddc72638609810dc5d22ddd894cb53760856796e
ValentinePumpkins <quignon.val@gmail.com>
2023-07-21 14:14:49 +0200

    ASM File for 64bites

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,21 @@
+%define newline 10
+%define sys_write 1
+%define stdout 1
+%define sys_exit 60
+%define success 0
+
+section .data
+	message db 'Hello, World', newline
+
+section .text
+	global main
+	main:
+		mov rax, sys_write
+		mov rdi, stdout
+		mov rsi, message
+		mov rdx, 12+1
+		syscall
+
+		mov rax, sys_exit
+		mov rdi, success
+		syscall
