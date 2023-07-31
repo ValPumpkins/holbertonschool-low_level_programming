c35a990353bd9de772102e50d83c57ecec53aead
ValentinePumpkins <quignon.val@gmail.com>
2023-07-31 14:59:54 +0200

    Create func that return nb of elt in dlistint_t for task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the number of elements in a linked dlistint_t list
+* @h: pointer to first node
+* Return: nb of elts
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *currentNode = h;
+	size_t countNode = 0;
+
+	while (currentNode  != NULL)
+	{
+		currentNode = currentNode->next;
+		countNode++;
+	}
+	return (countNode);
+}
