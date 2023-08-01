ebb3fe99c23f8771854ae031e66e2fab7d552c7a
ValentinePumpkins <quignon.val@gmail.com>
2023-08-01 13:32:27 +0200

    Task 7 : insert a  new node at idx

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: pointer to pointer to first node
+* @idx: index where to add new node
+* @n: value to add to new node
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *tempNode;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return(add_dnodeint(h, n));
+
+	tempNode = *h;
+	while (tempNode != NULL && count < idx -1)
+	{
+		tempNode = tempNode->next;
+		count++;
+	}
+	if (tempNode == NULL || count < idx - 1)
+		return (NULL);
+
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = tempNode;
+	newNode->next = tempNode->next;
+
+	if (tempNode->next != NULL)
+		tempNode->next->prev = newNode;
+
+	tempNode->next = newNode;
+	return(newNode);
+}
