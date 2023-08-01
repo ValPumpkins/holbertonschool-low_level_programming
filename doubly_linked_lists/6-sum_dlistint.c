93f6777f3ba15d19fa273ad8357faf1f948f040a
ValentinePumpkins <quignon.val@gmail.com>
2023-08-01 10:28:04 +0200

    Task6 - updated Betty style

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+* sum_dlistint - returns the sum of all data (n) of a dlistint_t linked list
 * @head: pointer to first node
 * Return: sum of all data (n) or 0 if empty
 */
