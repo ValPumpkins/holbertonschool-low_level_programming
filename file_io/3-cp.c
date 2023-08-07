f6c9a2fe885e5861c5acf79e391482551a3a7a12
ValentinePumpkins <quignon.val@gmail.com>
2023-08-07 17:15:59 +0200

    Task3: change error message 97

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{	dprintf(STDERR_FILENO, "Usage: %s file_from_file_to\n", argv[0]);
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from_file_to\n");
 		exit(97);
 	}
 
