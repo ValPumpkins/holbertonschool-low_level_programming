60eb0500805697c0700e21652f20366dc78c7154
ValentinePumpkins <quignon.val@gmail.com>
2023-08-09 14:31:58 +0200

    Task6: delete & free hashtable

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - deletes a hash table
+* @ht: hash table
+* Return: always 0
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *currentNode, *nextNode;
+
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		currentNode = ht->array[i];
+		while (currentNode != NULL)
+		{
+			nextNode = currentNode->next;
+			free(currentNode->key);
+			free(currentNode->value);
+			free(currentNode);
+			currentNode = nextNode;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
